a825e91cf9717fbb667ed40ae178c55f3e151ced
amatyarojini <amatyarojini@gmail.com>
2017-06-08 17:54:08 -0500

    oddevenfunction

---
+++ odd_evenfunction.py
@@ -0,0 +1,7 @@
+def odd_even(a,b):
+ for i in range(a,b+1):
+  if i%2==0:
+   print ("Number is",i,". This is an even number")
+  elif i%2 != 0:
+   print ("number is",i,". This is an odd number")
+odd_even(1,15)
