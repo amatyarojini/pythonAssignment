b2782e345ddc0bf10a869c9c266cb90504f541f7
amatyarojini <amatyarojini@gmail.com>
2017-06-08 17:51:48 -0500

    ComparingArrays

---
+++ identical.py
@@ -0,0 +1,8 @@
+#list_1 = [1,2,5,6,2]
+#list_2 = [1,2,5,6,2]
+list_1 = ['celery','carrots','bread','milk']
+list_2 = ['celery','carrots','bread','cream']
+if list_1 == list_2:
+ print("The lists are same")
+elif list_1 != list_2:
+ print("The lists are not same")
